50207520f3a00215c27ab75d6aac393f21d0615a
Caitlin Herrmann <cmherrmann33@gmail.com>
2020-08-05 17:22:23 -0500

    I'm not sure what update I did here

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-orbit-list',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orbit-list.component.css']
 })
 export class OrbitListComponent implements OnInit {
+
+  @Input() satellites: Satellite[];
 
   constructor() { }
 
